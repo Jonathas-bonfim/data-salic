f81ae90f3dc25ca740a28711147eb52f5db986a9
Jonathas-bonfim <jonathas.santos.bonfim@gmail.com>
2022-05-15 13:57:18 -0300

    styled project

--- src/pages/home/Home.tsx
+++ src/pages/home/Home.tsx
@@ -1,5 +1,6 @@
 
 import './styles.scss';
+import favoriteIMG from '../../assets/images/favorite.png'
 
 import AliceCarousel from "react-alice-carousel";
 import "react-alice-carousel/lib/scss/alice-carousel.scss";
@@ -31,10 +32,116 @@ export function Home() {
     <>
       <Header />
 
-      <div>
-        <h1>Ver Outros projetos do Proponente</h1>
-        <AliceCarousel>
+      <div id="container">
+        <h1 className="title-carousel">Ver Outros projetos do Proponente</h1>
+        <AliceCarousel >
+          <section className="carousel">
+            <strong className="tag">Area</strong>
+            <h4>Nome do Projeto</h4>
+            <p className="localization">São Paulo ● SP</p>
+            <p className="description">
+              O presente projeto intenciona promover o resgate, a difusão
+              e o acesso à música de canto coral genuinamente brasileira
+              através da manutenção do corpo artístico Coral Laudade
+              durante o
+            </p>
 
+            <p className="approved-value title-value">
+              Aprovado
+              <strong className="value">R$ 100.000,00</strong>
+            </p>
+            <p className="title-value">
+              Captado&ensp;
+              <strong className="value">R$ 500.000,00</strong>
+            </p>
+
+            <div className="icons">
+              <button>ADICIONAR</button>
+              <img src={favoriteIMG} alt="Favorito" />
+            </div>
+
+          </section>
+
+          <section className="carousel">
+            <strong className="tag">Area</strong>
+            <h4>Nome do Projeto</h4>
+            <p className="localization">São Paulo ● SP</p>
+            <p className="description">
+              O presente projeto intenciona promover o resgate, a difusão
+              e o acesso à música de canto coral genuinamente brasileira
+              através da manutenção do corpo artístico Coral Laudade
+              durante o
+            </p>
+
+            <p className="approved-value title-value">
+              Aprovado
+              <strong className="value">R$ 100.000,00</strong>
+            </p>
+            <p className="title-value">
+              Captado&ensp;
+              <strong className="value">R$ 500.000,00</strong>
+            </p>
+
+            <div className="icons">
+              <button>ADICIONAR</button>
+              <img src={favoriteIMG} alt="Favorito" />
+            </div>
+
+          </section>
+
+          <section className="carousel">
+            <strong className="tag">Area</strong>
+            <h4>Nome do Projeto</h4>
+            <p className="localization">São Paulo ● SP</p>
+            <p className="description">
+              O presente projeto intenciona promover o resgate, a difusão
+              e o acesso à música de canto coral genuinamente brasileira
+              através da manutenção do corpo artístico Coral Laudade
+              durante o
+            </p>
+
+            <p className="approved-value title-value">
+              Aprovado
+              <strong className="value">R$ 100.000,00</strong>
+            </p>
+            <p className="title-value">
+              Captado&ensp;
+              <strong className="value">R$ 500.000,00</strong>
+            </p>
+
+            <div className="icons">
+              <button>ADICIONAR</button>
+              <img src={favoriteIMG} alt="Favorito" />
+            </div>
+
+          </section>
+
+          <section className="carousel">
+            <strong className="tag">Area</strong>
+            <h4>Nome do Projeto</h4>
+            <p className="localization">São Paulo ● SP</p>
+            <p className="description">
+              O presente projeto intenciona promover o resgate, a difusão
+              e o acesso à música de canto coral genuinamente brasileira
+              através da manutenção do corpo artístico Coral Laudade
+              durante o
+            </p>
+
+            <p className="approved-value title-value">
+              Aprovado
+              <strong className="value">R$ 100.000,00</strong>
+            </p>
+            <p className="title-value">
+              Captado&ensp;
+              <strong className="value">R$ 500.000,00</strong>
+            </p>
+
+            <div className="icons">
+              <button>ADICIONAR</button>
+              <img src={favoriteIMG} alt="Favorito" />
+            </div>
+
+          </section>
         </AliceCarousel>
       </div>
     </>
